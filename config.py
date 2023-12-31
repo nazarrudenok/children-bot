f72f8e6ccc2745cd3bd77cc79a607ce7365e40a5
Nazar Rudenok <122445773+nazarrudenok@users.noreply.github.com>
2023-12-31 12:29:38 +0200

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ import pymysql
 TOKEN = '6501337482:AAGQPejyBdZLl27c1W7XPQKjDBmHw7e6TI8'
 
 connection = pymysql.connect(
-    host='localhost',
-    user='root',
-    password='',
-    database='childs'
+    host='mysql://children-user:HaDQ1xA8vW3j@srv-captain--crmojtdafy-mysql-80x:3306/children-database',
+    user='children-user',
+    password='HaDQ1xA8vW3j',
+    database='children-database'
 )
 
 cursor = connection.cursor()
